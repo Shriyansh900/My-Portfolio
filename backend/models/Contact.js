3a7ffbf1b7bcda628520e53caab66545739f0a21
Shriyansh Ozarkar <sozarkar7@gmail.com>
2025-05-18 23:55:12 +0530

    minor changes

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -1,32 +1,35 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please add a name'],
-    trim: true
+    required: [true, "Please add a name"],
+    trim: true,
   },
   email: {
     type: String,
-    required: [true, 'Please add an email'],
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
+    required: [true, "Please add an email"],
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
   },
   subject: {
     type: String,
-    required: [true, 'Please add a subject'],
-    trim: true
+    required: [true, "Please add a subject"],
+    trim: true,
   },
   message: {
     type: String,
-    required: [true, 'Please add a message'],
-    trim: true
+    required: [true, "Please add a message"],
+    trim: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 export default Contact;
