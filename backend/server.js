195571f31ded5eced2ed8815f0ec5926b86674f1
Shriyansh Ozarkar <sozarkar7@gmail.com>
2025-05-19 00:03:38 +0530

    port changes

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.get("/health", (req, res) => {
 // Routes
 app.use("/api/contact", contactRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2001;
 
 app.listen(PORT, () => {
   console.log(
